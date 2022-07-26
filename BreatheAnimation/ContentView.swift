21cc1d466f2bcb32598db90d31a9c74d2fb6ed50
Byron Mejia <isitsmt-4118@ucan.edu.ni>
2022-07-26 10:35:56 -0600

    Initial commit

---
+++ BreatheAnimation/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  BreatheAnimation
+//
+//  Created by Byron Mejia on 7/26/22.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
